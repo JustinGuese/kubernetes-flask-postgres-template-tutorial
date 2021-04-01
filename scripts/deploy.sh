23b34e878de9a7263da62b91691f2eb676104c56
Justin Guese <guese.justin@gmail.com>
2021-04-01 14:41:11 +0200

    travis v0.1

---
+++ scripts/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
+/usr/local/bin/kubectl \
+  --kubeconfig=/dev/null \
+  --server=$KUBERNETES_SERVER \
+  --certificate-authority=cert.crt \
+  --token=$KUBERNETES_TOKEN \
+  apply -f ./k8sversion/
+echo "The build number is ${TRAVIS_BUILD_NUMBER}"
+/usr/local/bin/kubectl \
+  --kubeconfig=/dev/null \
+  --server=$KUBERNETES_SERVER \
+  --certificate-authority=cert.crt \
+  --token=$KUBERNETES_TOKEN \
+  set image deployment/carflask pypytest=guestros/carflask:${TRAVIS_BUILD_NUMBER} --record
