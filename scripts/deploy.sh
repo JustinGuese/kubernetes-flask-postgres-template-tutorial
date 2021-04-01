130760c05eec6c2c3e64bc3e546d092bf764f2c7
Justin Guese <guese.justin@gmail.com>
2021-04-01 15:02:22 +0200

    kubectl fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
 # install nginx ingress for aws
-/usr/local/bin/kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.44.0/deploy/static/provider/aws/deploy.yaml
+/usr/local/bin/kubectl \
+  --kubeconfig=/dev/null \
+  --server=$KUBERNETES_SERVER \
+  --certificate-authority=cert.crt \
+  --token=$KUBERNETES_TOKEN \
+  apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.44.0/deploy/static/provider/aws/deploy.yaml
+# now start real build
 /usr/local/bin/kubectl \
   --kubeconfig=/dev/null \
   --server=$KUBERNETES_SERVER \
