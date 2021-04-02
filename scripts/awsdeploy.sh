ec43e05618ff170a01f0f02752de41c1c45af1cc
Justin Guese <guese.justin@gmail.com>
2021-04-02 12:37:45 +0200

    scrap all ideas and restart

--- scripts/awsdeploy.sh
+++ scripts/awsdeploy.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 # kubectl create secret generic pgpassword --from-literal='PGPASSWORD=''
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.44.0/deploy/static/provider/aws/deploy.yaml
+kubectl create namespace cert-manager
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+helm install \
+  cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --version v1.2.0 \
+  --create-namespace
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.2.0/cert-manager.yaml
 kubectl apply -f k8sversion
