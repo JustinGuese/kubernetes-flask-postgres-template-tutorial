73b7ba0600646cca4be6db06ca0db58d194cbc4a
Justin Guese <guese.justin@gmail.com>
2021-04-02 13:15:30 +0200

    stuck

--- scripts/awsdeploy.sh
+++ scripts/awsdeploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # kubectl create secret generic pgpassword --from-literal='PGPASSWORD=''
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.44.0/deploy/static/provider/aws/deploy.yaml
-kubectl create namespace cert-manager
+helm repo add nginx-stable https://helm.nginx.com/stable
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
+helm install nginx-ing nginx-stable/nginx-ingress --set rbac.create=true
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.44.0/deploy/static/provider/aws/deploy.yaml
+kubectl create namespace cert-manager
 helm install \
   cert-manager jetstack/cert-manager \
   --namespace cert-manager \
